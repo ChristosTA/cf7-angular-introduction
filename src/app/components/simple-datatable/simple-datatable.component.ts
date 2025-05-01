6e96a2326aafdb9b619055cd02377ec832bfafe1
Christos Tartaris <xristosbnd@gmail.com>
2025-05-01 11:49:07 +0300

    added:component-output-example

--- src/app/components/simple-datatable/simple-datatable.component.ts
+++ src/app/components/simple-datatable/simple-datatable.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, output } from '@angular/core';
 import { EPerson } from 'src/app/shared/interfaces/eperson';
 import {sortBy} from 'lodash-es';
 
@@ -10,6 +10,7 @@ import {sortBy} from 'lodash-es';
 })
 export class SimpleDatatableComponent {
   @Input() data: EPerson[] | undefined;
+  @Output() personClicked = new EventEmitter<EPerson>()
 
   sortOrder = {
     givenName: 'none',
@@ -46,7 +47,7 @@ export class SimpleDatatableComponent {
 
   onPersonClicked(person:EPerson){
     console.log("Person>>",person);
-    // this.personClicked.emit(person);
+    this.personClicked.emit(person);
   }
 
 }
